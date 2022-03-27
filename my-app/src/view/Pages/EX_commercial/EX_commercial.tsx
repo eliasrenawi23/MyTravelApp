5ecdbf7f345b558ac29a71e900871b5246f06e2f
Elias Re <eliasrenawi23@gmail.com>
2022-03-27 21:05:56 +0300

    ..

--- my-app/src/view/Pages/EX_commercial/EX_commercial.tsx
+++ my-app/src/view/Pages/EX_commercial/EX_commercial.tsx
@@ -37,7 +37,7 @@ const EX_commercial = () => {
     setAddress(hotel.hotelinfo.adress);
     setName(hotel.hotelinfo.name);
     setPhoto(hotel.hotelinfo.Photo);
-
+  console.log(hotel);
   }, []);
 
 
