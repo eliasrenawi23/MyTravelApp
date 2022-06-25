6f53b6101abcaa166320198d0c3ea19b6e5a3d14
Elias Re <eliasrenawi23@gmail.com>
2022-06-25 14:07:14 +0300

    loginvaldeate

--- my-app/src/view/Pages/Login/Login.tsx
+++ my-app/src/view/Pages/Login/Login.tsx
@@ -5,7 +5,7 @@ import Link from '@mui/material/Link';
 import { useLocation, useNavigate } from 'react-router-dom';
 import { useAppDispatch, useAppSelector } from '../../../app/hooks';
 import { changeNavText } from '../../../app/reducer/NavTextReducer';
-import { GetUser,loginAsync, loginWithGoogle } from '../../../app/reducer/UserReducer';
+import { GetUser, loginAsync, loginWithGoogle } from '../../../app/reducer/UserReducer';
 import './Login.scss';
 import 'react-cookie';
 
@@ -52,15 +52,51 @@ function Login(props: any) {
 
     }, [dispatch, user]);
 
+    function validate_emil(email: string) {
+        var apos = email.indexOf("@");
+        var dotop = email.lastIndexOf(".");
+        if (apos < 1 || dotop - apos < 2)
+            return false;
+        return true;
+    }
+    const validatePassword=(pass:string)=>{
+        var error="";
+        var illegalChars = /[\W_]/; // allow only letters and numbers
 
+        if(pass==""){
+            error="the password is empty";
+            alert(error)
+            return false;
+        } 
+        if(pass.length<7||pass.length>15){
+            error="the password length is not okey";
+            alert(error)
+            return false;
+        }
+        if(illegalChars.test(pass)){
+            error = "The password contains illegal characters.\n";
+            alert(error)
+            return false;
+        }
+        if(pass.search(/[a-zA-Z]+/)==-1||pass.search(/[0-9]+/)==-1){
+            error = "The password must contain at least one numeral.\n";
+            alert(error);
+            return false;
+        }
+        return true
+
+    }
     function loginhandle(e: any) {
         console.log("login pressed");
+
 
         if (Email === "" || Password === "") {
             alert("The username or email field is empty.");
         }
-
-
+        if (validate_emil(Email)) {
+            alert("The email is not okey.");
+        }
+        validatePassword(Password);
         console.log("Email : " + Email + "+" + "Password :" + Password)
         dispatch(loginAsync({
             Email: Email,
@@ -83,7 +119,7 @@ function Login(props: any) {
             ProfileImg: googledate.profileObj.imageUrl,
             Id: googledate.profileObj.googleId,
             Password: "googlepassword",
-            token:googledate.tokenId
+            token: googledate.tokenId
         }));
     }
     const handlelogout = (googledate: any) => {
